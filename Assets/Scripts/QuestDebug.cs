66c11dc655c3057ab6eab81bdb5b1deead1d0f94
JMulligan94 <j.mulligan94@hotmail.co.uk>
2020-05-07 18:17:56 +0100

    Initial Commit
    Populating repository

---
+++ Assets/Scripts/QuestDebug.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestDebug : MonoBehaviour
+{
+    public static QuestDebug Instance;
+    private Text m_text;
+    private bool m_inMenu;
+    public Transform m_rig;
+
+    private void Awake()
+    {
+        Instance = this;    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  RectTransform labelRect = DebugUIBuilder.instance.AddLabel( "Label" );
+      //  m_text = labelRect.GetComponent<Text>();
+        
+        m_inMenu = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    if ( OVRInput.GetDown( OVRInput.Button.Two ) || OVRInput.GetDown( OVRInput.Button.Start ) )
+        {
+            if ( m_inMenu )
+            {
+               // DebugUIBuilder.instance.Hide();
+            }
+            else
+            {
+               // DebugUIBuilder.instance.transform.position = m_rig.transform.TransformPoint( new Vector3( 0, 0, 4 ) );
+                //DebugUIBuilder.instance.transform.rotation = m_rig.transform.rotation;
+               // DebugUIBuilder.instance.Show();
+            }
+            m_inMenu = !m_inMenu;
+        }
+    }
+
+    public static void Log( string message )
+    {
+        //Instance.m_text.text = message;
+        Debug.Log( message );
+    }
+}
---
+++ Assets/Scripts/XRInputManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRInputManager : MonoBehaviour
+{
+	private List<InputDevice> m_inputDevices;
+
+	// This function is called when the object becomes enabled and active.
+	void OnEnable()
+	{
+		m_inputDevices = new List<InputDevice>();
+		// Get all current input devices and register
+		List<InputDevice> allCurrentDevices = new List<InputDevice>();
+		InputDevices.GetDevices( allCurrentDevices );
+		foreach ( var device in allCurrentDevices )
+		{
+			OnInputDeviceConnected( device );
+		}
+
+		// Register callbacks for devices connecting/disconnecting
+		InputDevices.deviceConnected += OnInputDeviceConnected;
+		InputDevices.deviceDisconnected += OnInputDevicesDisconnected;
+	}
+
+	// This function is called when the behaviour becomes disabled.
+	// This is also called when the object is destroyed and can be used for any cleanup code.When scripts are reloaded after compilation has finished, OnDisable will be called, followed by an OnEnable after the script has been loaded.
+	void OnDisable()
+	{
+		// Unregister callbacks for devices connecting/disconnecting
+		InputDevices.deviceConnected -= OnInputDeviceConnected;
+		InputDevices.deviceDisconnected -= OnInputDevicesDisconnected;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		bool buttonDownState = false;
+		foreach ( InputDevice inputDevice in m_inputDevices )
+		{
+			bool buttonPressedState = false;
+			if ( inputDevice.TryGetFeatureValue( CommonUsages.primaryButton, out buttonPressedState ) )
+				buttonDownState |= buttonPressedState; 
+		}
+
+		if ( buttonDownState )
+			QuestDebug.Log( "A button has been pressed!" );
+	
+	}
+
+	private void OnInputDeviceConnected( InputDevice obj )
+	{
+		m_inputDevices.Add( obj );
+	}
+
+	private void OnInputDevicesDisconnected( InputDevice obj )
+	{
+		if (m_inputDevices.Contains(obj))
+			m_inputDevices.Remove( obj );
+	}
+}
